721a74bd5c9bf85fbf17288d3fbe01e380ea15bf
MD SHAHADAT HOSSAIN <shahadat@media-365.com>
2019-08-03 15:32:15 +0600

    Current link updated

--- eventPage.js
+++ eventPage.js
@@ -8,15 +8,22 @@ var apiUrl = 'http://daynight.bahramkhan.com/api/store-word'
 chrome.contextMenus.create(menuItem);
 
 chrome.contextMenus.onClicked.addListener(function(clickData){
-
-    if (clickData.menuItemId === "add_to_my_word" && clickData.selectionText){
+    var url;
+    chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    }, function(tabs) {
+        var tab = tabs[0];
+        var url = tab.url;
+        if (clickData.menuItemId === "add_to_my_word" && clickData.selectionText){
 
             $.post(apiUrl,
                 {
                     word: clickData.selectionText,
-                    source: window.location.href,
+                    source: url,
                 }
             );
 
-    }
+        }
+    });
 });
